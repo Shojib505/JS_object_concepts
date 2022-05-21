61ea372e5e5d0c3b6aba21b633930cb7e145b887
alish <alishojib12@gmail.com>
2022-05-21 23:45:07 +0600

    object cratetion on JS

---
+++ object-create.js
@@ -0,0 +1,19 @@
+//1. using object literal
+const student = { name: 'Sojib All', job: 'developer' }
+//2. constructor
+const person = new Object();
+
+//3. 
+const human = Object.create(student);
+console.log(human.job)
+
+//4.
+class People {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const peop = new People('Manush', 23)
+console.log(peop)
+
